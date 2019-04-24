1cc5e3607f2d1799480de4fda05ccee53ff68b8c
Xiangyu Alaric Wei <35881947+AECassAWei@users.noreply.github.com>
2019-04-24 16:38:53 -0700

    title add

--- extractFabricatedPapers.py
+++ extractFabricatedPapers.py
@@ -9,6 +9,8 @@ with open('fabricatedPapers.csv', 'w') as csv_file:
 	writer.writeheader()
 	db = open('RetractionWatchDatabase_SOC.htm', 'rb')
 	soup = BeautifulSoup(db, 'html.parser')
+	for t in soup.find_all('span', attrs={'class':'rTitleNotIE'}):
+		print(t.get_text())
 
 	csv_file.close()
 	db.close()
