e0b45f23c0f8d9e22dea094144bfd0b58e5147a0
Xiangyu Alaric Wei <35881947+AECassAWei@users.noreply.github.com>
2019-05-01 01:30:50 -0700

    scrape year, authors, title, journal

--- extractFabricatedPapers.py
+++ extractFabricatedPapers.py
@@ -3,21 +3,59 @@ import csv
 import re
 
 
-with open('fabricatedPapers.csv', 'w') as csv_file:
-	fieldnames = ['year', 'authors', 'title', 'journal']
+with open('fabricatedPapers.csv', 'w', encoding='utf-8') as csv_file:
+	fieldnames = ['year publish', 'year retract', 'authors', 'title', 'journal']
 	writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
 	writer.writeheader()
 	db = open('RetractionWatchDatabase_SOC.htm', 'rb')
 	soup = BeautifulSoup(db, 'html.parser')
+	
+	for paper in soup.find_all('tr', attrs={'class':'mainrow'}):
+		
+		# find title
+		title = paper.find('span', attrs={'class':'rTitleNotIE'}).get_text()
+		# print(title)
+
+		# find journal
+		journal = paper.find('span', attrs={'class':"rJournal"})\
+			     .find('span', attrs={'class':"rJournal"}).get_text()[:-4]
+		# print(journal)
+		
+		# find year_published and year_retracted
+		year_text = paper.get_text(" ")
+		year_published = re.findall(r'(\d+/\d+/\d+)', year_text)[-2][-4:]
+		year_retracted = re.findall(r'(\d+/\d+/\d+)', year_text)[-1][-4:]
+		# print(year_published, year_retracted)
+
+		# find author
+		authors = []
+		for author in paper.find_all('a', attrs={'class':'authorLink'}):
+			authors.append(author.get_text())
+		# print(authors)
+
+		writer.writerow({'year publish': year_published, 'year retract': year_retracted, 'authors': authors, 'title': title, 'journal': journal})
+	
+	"""
 	for t in soup.find_all('span', attrs={'class':'rTitleNotIE'}):
-		print(t.get_text())
+	    print(t.get_text())
+	# print(title)
+
+	for temp in soup.find_all('span', attrs={'class':"rJournal"}):
+		for j in temp.find_all('span', attrs={'class':"rJournal"}):
+			print(j.get_text()[:-4])
+	# print(journal)
+
 
 	year = pass
 	authors = pass
-	title = pass
-	journal = pass
 
-	for i in rowOfTable:
+	title = soup.find('span', attrs={'class':'rTitleNotIE'}).get_text()
+	print(title)
+	# journal = pass
+
+	for i in db.rows():
 		writer.writerow({'year': year, 'authors': authors, 'title': title, 'journal': journal})
+	"""
+	
 	csv_file.close()
 	db.close()
