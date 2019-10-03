566556591b6c3768958bad87ab50891b3bb206c0
Xiangyu Alaric Wei <35881947+AECassAWei@users.noreply.github.com>
2019-10-03 16:06:43 -0700

    statcheckFabricatedPaper.R updated

--- statcheckFabricatedPaper.R
+++ statcheckFabricatedPaper.R
@@ -3,24 +3,22 @@ library(lubridate)
 library(dplyr)
 
 # set working directory
-setwd("E:/UCSD/Lab/Computational Cognition Lab/GitHub/Fabricator")
+# setwd("E:/UCSD/Lab/Computational Cognition Lab/GitHub/Fabricator")
+# set working directory
+setwd("E:/UCSD/Lab/Computational Cognition Lab/GitHub/Fabricator/")
 
-"""files <- list.files(path="htmls/clean", pattern="*.html", full.names=TRUE, recursive=TRUE)
-for (file in files){ # load file
-# apply function
-out <- checkHTML(file)
-# write to file
-write.table(out, file = "output.txt")
-}"""
+# read csv file
+paper <- read.csv(file = 'elsevier/cognition/extract_2010.csv', encoding = "UTF-8", header=TRUE, sep=",")
+nrow(paper)
 
+v1 <- paper[[3]]
+# result <- 0
 
-out <- checkdir("htmls/clean", subdir = FALSE)
-write.table(out, file = "outputFile.txt")
-write.csv(out, file = "outputFile.csv")
+for (row in 1:nrow(paper)) {
+  txt <- v1[row]
+  stat <- statcheck(txt)
+}
 
-
-print(checkHTML("htmls/clean/BrownCronkGrochow_2005_DanceRevealsSymmetry.html"))
-
-print(checkHTML("htmls/clean/One and Done_ Optimal Decisions From Very Few Samples - Vul - 2014 - Cognitive Science - Wiley Online Library.htm"))
-
-print(checkHTML("htmls/clean/AndersonAwh_2012_ThePlateauInMnemonicResolution.html"))
+txt <- v1[46]
+statcheck(v1[46])
+stat <- statcheck(v1[46])
